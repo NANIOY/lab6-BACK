b88cc920cbeb5bfc7813fa4799d77562348a75ff
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-02 17:52:33 +0100

    feat: add mongodb + key, mongoose, cors

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +18,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// enable CORS
+app.use(cors());
+
+// mongoDB Connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// check MongoDB connection status
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
